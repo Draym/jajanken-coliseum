90d1a4e6e31476fddececab509f7b4093aba7460
kevin <kevin.andres@epitech.eu>
2024-02-12 11:17:08 +0800

    task: remove tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Landing from "@/views/landing";
 
 export default function Home() {
-  return (
-    <main>
-
-    </main>
-  );
+    return (
+        <main>
+            <Landing></Landing>
+        </main>
+    );
 }
